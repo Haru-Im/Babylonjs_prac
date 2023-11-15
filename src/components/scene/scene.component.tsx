88f38588f936397f91626c4449b0c64149e1399e
Haru-Im <chalk323@naver.com>
2023-11-15 19:59:27 +0900

    basic scene component

--- src/components/scene/scene.component.tsx
+++ src/components/scene/scene.component.tsx
@@ -1,4 +1,12 @@
-import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3 } from '@babylonjs/core';
+import {
+  Color3,
+  Engine,
+  FreeCamera,
+  MeshBuilder,
+  PointLight,
+  Scene,
+  Vector3,
+} from '@babylonjs/core';
 import { FC, useEffect, useRef } from 'react';
 import styles from './scene.component.module.css';
 
@@ -12,38 +20,68 @@ export const SceneComponent: FC<ISceneComponentProps> = ({}) => {
 
     if (!canvas) return;
 
-    // antialias option 활성화
-    const engine = new Engine(canvas, true);
+    const createEngine = () => {
+      const engine = new Engine(canvas, true);
 
-    const scene = new Scene(engine);
+      engine.runRenderLoop(() => {
+        if (typeof onRender === 'function') onRender(scene);
+        scene.render();
+      });
 
-    const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
-    camera.setTarget(Vector3.Zero());
+      return engine;
+    };
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+    const createScene = (engine) => {
+      const scene = new Scene(engine);
 
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+      return scene;
+    };
 
-    const box = MeshBuilder.CreateBox('box', { size: 2 }, scene);
-    box.position.y = 1;
+    const createCamera = () => {
+      const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
+      camera.setTarget(Vector3.Zero());
+      // 방향키로 조절이 가능하도록
+      camera.attachControl(canvas, true);
 
-    MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene);
+      return camera;
+    };
+
+    const createLight = () => {
+      const light = new PointLight('light', new Vector3(0, 10, 0), scene);
+      light.intensity = 0.7;
+      light.diffuse = new Color3(1, 0, 0);
+
+      return light;
+    };
+
+    const createMesh = () => {
+      const sphere = MeshBuilder.CreateSphere('sphere', {});
+      sphere.position.y = 1;
+
+      return { sphere };
+    };
+
+    const createGround = () => {
+      const ground = MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene);
+
+      return ground;
+    };
+
+    const engine = createEngine();
+    const scene = createScene(engine);
+    const camera = createCamera();
+    const light = createLight();
+    const { sphere } = createMesh();
+    const ground = createGround();
 
     const onRender = (scene: Scene) => {
-      if (box !== undefined) {
+      if (sphere !== undefined) {
         const deltaTimeInMillis = scene.getEngine().getDeltaTime();
 
         const rpm = 10;
-        box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+        sphere.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
       }
     };
-
-    engine.runRenderLoop(() => {
-      if (typeof onRender === 'function') onRender(scene);
-      scene.render();
-    });
 
     const resize = () => {
       scene.getEngine().resize();
